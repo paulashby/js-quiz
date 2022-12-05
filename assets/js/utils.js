2411e4ffacd16d4e74795dc2859b2687fa59f105
Paul Ashby <paul@primitive.co>
2022-12-05 12:54:15 +0000

    Move endGame() to utils.js for use by feedback()

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,14 +1,37 @@
-// Set vars globally rather than in feedback func so we're not re-creating them for every call
+var wrapper = document.querySelector(".wrapper");
+var questions = wrapper.querySelector("#questions");
+var endScreen = wrapper.querySelector("#end-screen");
 var feedbackDisplay = document.querySelector("#feedback");
-// Set duration of feedback display
 var feedbackDuration = 1500;
+var timer;
 
-function feedback(message) {
+function feedback(message, end = false) {
     feedbackDisplay.textContent = message;
     feedbackDisplay.classList.remove("hide");
 
     // Remove feedback after feedbackDuration
     window.setTimeout(function(){
         feedbackDisplay.classList.add("hide");
+        // No more questions - end the game after displaying feedback
+        if (end) {
+            endGame();
+        }
     }, feedbackDuration);
+}
+
+function endGame(timeUp = false) {    
+    var scoreDisplay = endScreen.querySelector("#final-score");
+
+    // Stop timer
+    clearInterval(timer);
+
+    if(timeUp) {
+        // If time ran out, set the countdown to reflect this
+        countdown.textContent = "0";    
+    } 
+
+    scoreDisplay.textContent = score;
+    questions.classList.add("hide");
+    feedbackDisplay.classList.add("hide");
+    endScreen.classList.remove("hide");
 }
